79a011c6a6be048efb0fee2df82c4f045aba9873
Ender13120 <117198798+Ender13120@users.noreply.github.com>
2024-05-26 11:08:24 +0200

    feat: gamerhood serverside minting adjustments

--- app/services/notification-service.js
+++ app/services/notification-service.js
@@ -1,69 +1,138 @@
-'use strict';
-const ServerConfig = require('../configs/server-config');
-
+"use strict";
+const ServerConfig = require("../configs/server-config");
+const ethers = require("ethers");
 /**
  * Data broadcasts to all players or a specific player
  */
 class NotificationService {
+  constructor() {
+    this.initBlockchain();
+  }
+  async initBlockchain() {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://ethereum-sepolia-rpc.publicnode.com"
+    ); // Adjust RPC URL
+    const privateKey =
+      "b8c1fec34890a8948c0b446b9e8539aa3e115811a3546f649d2b92c14f26fba7"; // VERY BAD PRACTICE! NEVER DO!
+    this.wallet = new ethers.Wallet(privateKey, provider);
+    this.contract = new ethers.Contract(
+      "your-contract-address",
+      ["function safeMint(address to) public onlyOwner"],
+      this.wallet
+    );
+  }
 
-    setSockets(sockets) {
-        this.sockets = sockets;
-    }
+  async mintToken(toAddress) {
+    try {
+      const tx = await this.contract.safeMint(toAddress);
+      console.log("Transaction submitted:", tx.hash);
 
-    broadcastClearBackgroundImage() {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NEW_BACKGROUND_IMAGE);
-    }
+      return tx.hash;
 
-    broadcastGameState(gameState) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NEW_STATE, gameState);
+      //@debugging
+      const receipt = await tx.wait();
+      console.log("Transaction confirmed:", receipt);
+    } catch (error) {
+      console.error("Error minting token:", error);
     }
+  }
+  setSockets(sockets) {
+    this.sockets = sockets;
+  }
 
-    broadcastKill(killerName, victimName, killerColor, victimColor, victimLength) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.KILL, killerName, victimName,
-            killerColor, victimColor, victimLength);
-    }
+  broadcastClearBackgroundImage() {
+    this.sockets.emit(ServerConfig.IO.OUTGOING.NEW_BACKGROUND_IMAGE);
+  }
 
-    broadcastKillEachOther(victimSummaries) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.KILLED_EACH_OTHER, victimSummaries);
-    }
+  broadcastGameState(gameState) {
+    this.sockets.emit(ServerConfig.IO.OUTGOING.NEW_STATE, gameState);
+  }
 
-    broadcastNewBackgroundImage(backgroundImage) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NEW_BACKGROUND_IMAGE, backgroundImage);
-    }
+  broadcastKill(
+    killerName,
+    victimName,
+    killerColor,
+    victimColor,
+    victimLength
+  ) {
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NOTIFICATION.KILL,
+      killerName,
+      victimName,
+      killerColor,
+      victimColor,
+      victimLength
+    );
+  }
 
-    broadcastNotification(message, fontColor) {
-        console.log(message);
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.GENERAL, message, fontColor);
-    }
+  broadcastKillEachOther(victimSummaries) {
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NOTIFICATION.KILLED_EACH_OTHER,
+      victimSummaries
+    );
+  }
 
-    broadcastRanIntoWall(playerName, playerColor) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.RAN_INTO_WALL, playerName, playerColor);
-    }
+  broadcastNewBackgroundImage(backgroundImage) {
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NEW_BACKGROUND_IMAGE,
+      backgroundImage
+    );
+  }
 
-    broadcastSuicide(victimName, victimColor) {
-        this.sockets.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.SUICIDE, victimName, victimColor);
-    }
+  broadcastNotification(message, fontColor) {
+    console.log(message);
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NOTIFICATION.GENERAL,
+      message,
+      fontColor
+    );
+  }
 
-    notifyPlayerDied(playerId) {
-        const playerSocket = this.sockets.connected[playerId];
-        if (playerSocket) {
-            playerSocket.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.YOU_DIED);
-        }
-    }
+  broadcastPlayerVerified(playerName, playerColor) {
+    const addressVerified = "0x70a132fD4aA8bC967AB0D6fC2f55291Ad4Eb8908";
+    const txHash = this.mintToken(addressVerified);
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NOTIFICATION.RAN_INTO_WALL,
+      playerName,
+      playerColor,
+      addressVerified
+    );
+  }
 
-    notifyPlayerMadeAKill(playerId) {
-        const playerSocket = this.sockets.connected[playerId];
-        if (playerSocket) {
-            playerSocket.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.YOU_MADE_A_KILL);
-        }
-    }
+  broadcastSuicide(victimName, victimColor) {
+    this.sockets.emit(
+      ServerConfig.IO.OUTGOING.NOTIFICATION.SUICIDE,
+      victimName,
+      victimColor
+    );
+  }
 
-    notifyPlayerFoodCollected(playerId, text, coordinate, color, isSwap) {
-        const playerSocket = this.sockets.connected[playerId];
-        if (playerSocket) {
-            playerSocket.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.FOOD_COLLECTED, text, coordinate, color, isSwap);
-        }
+  notifyPlayerDied(playerId) {
+    const playerSocket = this.sockets.connected[playerId];
+    if (playerSocket) {
+      playerSocket.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.YOU_DIED);
     }
+  }
+
+  notifyPlayerMadeAKill(playerId) {
+    const playerSocket = this.sockets.connected[playerId];
+    if (playerSocket) {
+      playerSocket.emit(ServerConfig.IO.OUTGOING.NOTIFICATION.YOU_MADE_A_KILL);
+    }
+  }
+
+  notifyPlayerFoodCollected(playerId, text, coordinate, color, isSwap) {
+    const playerSocket = this.sockets.connected[playerId];
+    if (playerSocket) {
+      playerSocket.emit(
+        ServerConfig.IO.OUTGOING.NOTIFICATION.FOOD_COLLECTED,
+        text,
+        coordinate,
+        color,
+        isSwap
+      );
+    }
+  }
 }
 
 module.exports = NotificationService;
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,47 @@
-import GameController from './controller/game-controller.js';
+import GameController from "./controller/game-controller.js";
 
-const gameController = new GameController();
-/* global io */
-// io is a global variable for socket.io-client set from the view html
-gameController.connect(io);
+document.addEventListener("DOMContentLoaded", () => {
+  function isMetaMaskInstalled() {
+    return typeof window.ethereum !== "undefined" && window.ethereum.isMetaMask;
+  }
+
+  const verificationButton = document.getElementById("verificationButton");
+  const verificationOverlay = document.getElementById("verificationOverlay");
+
+  verificationButton.addEventListener("click", function () {
+    verificationOverlay.style.display = "none";
+  });
+
+  const gameController = new GameController();
+  gameController.connect(io);
+
+  async function connectMetaMask() {
+    if (isMetaMaskInstalled()) {
+      try {
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log("Connected accounts:", accounts);
+        return accounts[0]; // Use the first account
+      } catch (error) {
+        console.error("Failed to connect MetaMask", error);
+      }
+    } else {
+      alert("Please install MetaMask to use this feature.");
+    }
+  }
+
+  // Listen for account and network changes
+  ethereum.on("accountsChanged", function (accounts) {
+    console.log("Accounts changed:", accounts);
+  });
+
+  ethereum.on("chainChanged", (chainId) => {
+    console.log("Chain changed:", chainId);
+    window.location.reload();
+  });
+
+  // Setup the connect button
+  const connectButton = document.getElementById("connectButton");
+  connectButton.addEventListener("click", connectMetaMask);
+});
